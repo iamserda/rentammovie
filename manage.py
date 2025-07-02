5a0292754dc07a43d0ddeac013053a513060c28c
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-07-02 18:03:31 -0400

    changed default port to a different to avoid issues with any other django app one may be running with the default port number. Not dynamic for now. -@iamserda

--- manage.py
+++ manage.py
@@ -3,10 +3,12 @@
 import os
 import sys
 
+from django.core.management.commands.runserver import Command as runserver
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vidlyap.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vidlyap.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_PORT", "8882")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -19,4 +21,5 @@ def main():
 
 
 if __name__ == '__main__':
+    runserver.default_port = "8882"
     main()
