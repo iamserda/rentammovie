85b84aa80189ce1eddd870b39cb6ce43dfe7698b
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-07-02 18:06:49 -0400

    new templates with specific jobs like showing error, show movie or movies, etc. playing with templates. I think they are awful beyond prototyping. more difficult than its worth. good idea for whoever started it but for developers who are already familiar with common frontend patterns, this is diabolical-@iamserda

--- movies/views.py
+++ movies/views.py
@@ -5,12 +5,13 @@ from .models import Movie
 # Create your views here.
 def handle_movie_home(request:HttpRequest):
     """ Function handles request for domain/movies"""
-    movies =  Movie.objects.all().order_by("title")
+    movies = Movie.objects.all().order_by("title")
+    return render(request, "movies/movie.html", {"movies": movies})
 
-    return render(request, "movies/index.html", {"movies": movies})
 
-def handle_movie_by_id(request, movie_id):
+def handle_movie_by_id(request: HttpRequest, movie_id):
     """ Function handles request for domain/movies"""
-    if movie_id:
-        return HttpResponse(content=f"<h1>You are Movie #{movie_id}'s page!</h1>")
-    return HttpResponse(content=f"<h1>You are Temporary Movie page!</h1>")
+    movie = [movie for movie in Movie.objects.all() if movie.id == movie_id]
+    if movie:
+        return render(request, "movies/movie.html", {"movies": movie})
+    return render(request, "movies/error.html", {"movies": movie})
